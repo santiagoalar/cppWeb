bf2b5e1ec9ec1a8813fb43c72045dfb856206109
TIBAQUIRA <tibaquirajoan@outlook.com>
2025-05-21 17:11:29 -0500

    Latest commit

--- src/app/basic/main-page/main-page.component.ts
+++ src/app/basic/main-page/main-page.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import { Inject } from '@angular/core';
+import { VersionService } from 'src/app/modules/services/version.service';
 
 @Component({
   selector: 'app-main-page',
@@ -10,10 +11,18 @@ import { Inject } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
-  constructor() {
+  appVersion: string;
+  
+  constructor(
+    private router: Router,
+    private translate: TranslateService,
+    private authService: AuthService,
+    private versionService: VersionService
+  ) {
+    this.appVersion = this.versionService.getVersion();
   }
 
   ngOnInit(): void {
-      
+    console.log(`App version: ${this.appVersion}`);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
     apiBaseUrl: 'https://api.misw4502-ccp.com',
+      version: '1.4.0',
   };
